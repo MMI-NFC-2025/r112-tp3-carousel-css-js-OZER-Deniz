b90a6403271c93cb54f32f2301b8916f495c31c9
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2025-11-05 08:52:52 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,23 @@
 // *** CAROUSEL ***
-// Implémentation de base: défilement précédent / suivant (valeur fixe)
+// Défilement précédent / suivant avec calcul dynamique de la largeur d'un item
 
 const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 
 if (carousel) {
-    const scrollAmount = 260; // valeur de base fixe pour le test initial
+    // Déterminer dynamiquement la largeur d'un item
+    const getScrollAmount = () => {
+        const premierItem = document.querySelector(".carousel__item");
+        return premierItem ? premierItem.clientWidth : 260;
+    };
+
+    let scrollAmount = getScrollAmount();
+
+    // Mettre à jour la valeur au redimensionnement
+    window.addEventListener("resize", () => {
+        scrollAmount = getScrollAmount();
+    });
 
     // Scroll au clic sur le bouton précédent
     if (prevButton) {
