1359fa83ada0fac1966950512c748143c2914add
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2025-11-05 08:52:09 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,24 @@
 // *** CAROUSEL ***
-// TODO
+// Implémentation de base: défilement précédent / suivant (valeur fixe)
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+if (carousel) {
+    const scrollAmount = 260; // valeur de base fixe pour le test initial
+
+    // Scroll au clic sur le bouton précédent
+    if (prevButton) {
+        prevButton.addEventListener("click", () => {
+            carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+        });
+    }
+
+    // Scroll au clic sur le bouton suivant
+    if (nextButton) {
+        nextButton.addEventListener("click", () => {
+            carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+        });
+    }
+}
